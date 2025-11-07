0891a914e0d34a9736d51dff0b3b1f41e87a7812
Joe Koop <joe@joekoop.com>
2025-11-07 00:43:20 +0000

    found some changes

--- src/views/footer.php
+++ src/views/footer.php
@@ -1,4 +1,5 @@
 <footer>
+<span style="color:red">this site needs to be rewritten</span><br>
     Authum<?= '' /* '/' . e(AUTHUM_VERSION) */ ?> -
     logged in as <?= e(loggedInUser()['name']) ?> <?php view('discord-icon-link', ['id' => loggedInUser()['id']]) ?>
 </footer>
--- src/views/head.php
+++ src/views/head.php
@@ -6,4 +6,5 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Slabo+27px&display=swap" rel="stylesheet">
 <title><?= e($title) ?> - Authum</title>
+<script defer src="https://a.joekoop.com/script.js" data-website-id="9a1c4b7c-2922-4a6b-8c86-fb770b1eee21"></script>
 <?= styleTag('main') ?>
